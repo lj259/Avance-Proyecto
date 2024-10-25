1f851ec6733fb47146d32c5576ac16fe8cffe253
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-10-25 14:46:52 -0600

    avanceenrutamiento

--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVistas;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('inicio',[ControladorVistas::class,'home'])->name('rutainicio');
+
+Route::get('/registro',[ControladorVistas::class,'registro'])->name('rutaregis');
+
+Route::get('/recupereacion',[ControladorVistas::class,'recuperacion'])->name('rutarecuperacion');
+
+Route::post('/forminicio',[ControladorVistas::class,'forminicio'])->name('forminicio');
+
+Route::post('/formregistro',[ControladorVistas::class,'forminicio'])->name('formregis');
+
+Route::post('/formrecuperacion',[ControladorVistas::class,'formrecuperacion'])->name('formrecuperecion');
