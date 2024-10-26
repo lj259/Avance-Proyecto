4c5ef59783bbadd5620e0eafd33f85ef1ec1346b
joaco2752 <122044319@upq.edu.mx>
2024-10-25 22:05:40 -0600

    validadorRegistro

--- Proyecto/app/Http/Controllers/Controller.php
+++ Proyecto/app/Http/Controllers/Controller.php
@@ -1,8 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\validadorRegistro;
 
 abstract class Controller
 {
-    //
+    public function procesarRegistro(validadorRegistro $peticion)
+    {
+        
+        $nombre = $peticion->input('txtnombre');
+        $apellido = $peticion->input('txtapellido');
+        $telefono = $peticion->input('txttelefono');
+        $correo = $peticion->input('txtcorreo');
+
+        session()->flash('exito', 'Se guardó el usuario: ' . $nombre . ' ' . $apellido);
+
+        return to_route('formregis');
+    }
 }
