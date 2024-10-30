d5443e32d37b557f6075d970f016890150beb70f
lj259 <120034783@upq.edu.mx>
2024-10-29 18:52:08 -0600

    merge ramas

--- Proyecto/app/Http/Requests/ValidadorRegistro.php
+++ Proyecto/app/Http/Requests/ValidadorRegistro.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ValidadorRegistro extends FormRequest
+class validadorRegistro extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class ValidadorRegistro extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'txtnombre' => 'required|min:7|max:20',
+            'txtapellido' => 'required|min:7|max:20',
+            'txttelefono' => 'required|numeric',
+            'txtcorreo' => 'email:rfc:dns'
         ];
     }
 }
