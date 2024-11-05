2c2cb71d12808acc1f32c54d592af8ecf2e8e708
lu22111 <139249445+lu22111@users.noreply.github.com>
2024-11-04 18:10:35 -0600

    rutas2

--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -7,11 +7,33 @@ Route::get('inicio',[ControladorVistas::class,'home'])->name('rutainicio');
 
 Route::get('/registro',[ControladorVistas::class,'registro'])->name('rutaregis');
 
-Route::get('/recupereacion',[ControladorVistas::class,'recuperacion'])->name('rutarecuperacion');
-
-Route::post('/forminicio',[ControladorVistas::class,'forminicio'])->name('forminicio');
-
-Route::post('/formregistro', [ControladorVistas::class, 'formregistro'])->name('formregis');
+Route::get('/recuperacion',[ControladorVistas::class,'recuperacion'])->name('rutarecuperacion');
 
 
-Route::post('/formrecuperacion',[ControladorVistas::class,'formrecuperacion'])->name('formrecuperecion');
+Route::post('/registro/cliente ', [ControladorVistas::class, 'registrocliente'])->name('registro/cliente');
+
+Route::get('/busqueda/hoteles',[ControladorVistas::class,'busquedahoteles'])->name('rutabuscahoteles');
+
+
+
+Route::get('/busqueda/vuelos',[ControladorVistas::class,'busquedavuelos,'])->name('rutabuscavuelos');
+
+Route::get('/carrito/reservacion',[ControladorVistas::class,'carritoreservacion'])->name('rutareservacion');
+
+Route::get('/Detalle/hotel',[ControladorVistas::class,'Detallehotel'])->name('rutahotel');
+
+Route::get('/login',[ControladorVistas::class,'login'])->name('login');
+
+Route::get('/metodopago',[ControladorVistas::class,'metodopago'])->name('metodopago');
+
+
+
+
+Route::get('/resultado/hotel',[ControladorVistas::class,'resultadohotel'])->name('resultadohotel');
+
+Route::get('/resultado/vuelo',[ControladorVistas::class,'resultadovuelo'])->name('resultadovuelo');
+
+
+
+
+
