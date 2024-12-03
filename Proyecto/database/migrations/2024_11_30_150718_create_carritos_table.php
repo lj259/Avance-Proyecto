d8815c5f012f0b2703ea689477c4c4c6ff75cef1
lj259 <120034783@upq.edu.mx>
2024-12-02 19:15:10 -0600

    correccion base de datos

--- Proyecto/database/migrations/2024_11_30_150718_create_carritos_table.php
+++ Proyecto/database/migrations/2024_11_30_150718_create_carritos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('carritos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->unsignedBigInteger('hotel_id');
+            $table->unsignedBigInteger('vuelo_id');
+            
             $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
             $table->foreign('hotel_id')->references('id')->on('hoteles')->onDelete('cascade');
             $table->foreign('vuelo_id')->references('id')->on('vuelos')->onDelete('cascade');
--- Proyecto/database/migrations/2024_11_30_150732_create_escalas_table.php
+++ Proyecto/database/migrations/2024_11_30_150732_create_escalas_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('escalas', function (Blueprint $table) {
             $table->id();
-            $table->string(column: 'destino',150);
+            $table->string( 'destino',150);
             $table->integer('hora_salida');//formato 24hr, convertir a 12hr
             $table->integer('hora_llegada');//formato 24hr, convertir a 12hr
+            $table->unsignedBigInteger('vuelo_id');
             $table->foreign('vuelo_id')->references('id')->on('vuelos')->onDelete('cascade');
             $table->timestamps();
         });
